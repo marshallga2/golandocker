25b4cfbf418e9793dd005b8ea19979acdcbe28c1
marshallga2 <garciamarshall2@outlook.com>
2024-07-16 14:55:02 -0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -13,8 +13,8 @@ cat << EOF > /usr/local/etc/trojan-go/config.json
 {
   "run_type": "server",
   "local_addr": "0.0.0.0",
-  "local_port": 10000,
-  "remote_addr": "yahoo.com",
+  "local_port": 443,
+  "remote_addr": "127.0.0.1",
   "remote_port": 80,
   "log_level": 5,
   "password": ["$UUID"],
